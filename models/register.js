62e9cec1123248ee3774ed8b4e816f8e8697d22f
pnga <phuongnga0pn@gmail.com>
2022-11-05 21:06:06 +0700

    register pnga

--- models/register.js
+++ models/register.js
@@ -1,5 +1,12 @@
 var token_cybersoft = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udGVuZCA3MyIsIkhldEhhblN0cmluZyI6IjIzLzA1LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY4NDgwMDAwMDAwMCIsIm5iZiI6MTY1OTg5MTYwMCwiZXhwIjoxNjg0OTQ3NjAwfQ.u471oZWr9EMgIb7oeyuaxfi8spgAgUuTkUHYSS9pBWg';
-import './employ';
+function Employ(){
+    this.email = '';
+    this.password='';
+    this.name='';
+    this.gender = '';
+    this.phone = '';
+    
+}
 
 document.querySelector('#btnSubmit').onclick = function (){
     var emp = new Employ();
@@ -8,8 +15,13 @@ document.querySelector('#btnSubmit').onclick = function (){
     emp.name = document.querySelector('#name').value;
     emp.phone = document.querySelector('#phone').value;
     // lay ra gia tri gioi tinh
-    var chkGender = document.getElementsByName('gender');
-    console.log(chkGender);
+    let male = document.querySelector('#male').checked;
+    let female = document.querySelector('#female').checked;
+    if(male=== true){
+        emp.gender=true;
+    }else{
+        emp.gender=false;
+    }
     var promise = axios({
         url :  `https://shop.cyberlearn.vn/api/Users/signup`,
         method:'POST',
